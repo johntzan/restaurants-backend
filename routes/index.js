bf32d50bcf4311a30aa91ee540e91b8561eb8fd0
johntzan <john.tzanida@gmail.com>
2018-03-23 03:36:04 -0400

    add privacy policy and tos for apps

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get("/", function(req, res, next) {
   res.sendFile("views/index.html", { root: __dirname });
 });
 
+router.get("/privacy-policy", function(req, res, next) {
+  res.sendFile("views/privacy-policy.html", { root: __dirname });
+});
+
+router.get("/terms", function(req, res, next) {
+  res.sendFile("views/tos.html", { root: __dirname });
+});
+
 module.exports = router;
